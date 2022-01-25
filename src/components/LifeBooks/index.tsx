aad8d0d76dc46a50899701a778f59232d5bb7940
M.W <auddnjs2008@naver.com>
2022-01-25 18:12:44 +0900

    books 페이지 작업중

--- src/components/LifeBooks/index.tsx
+++ src/components/LifeBooks/index.tsx
@@ -1,14 +1,74 @@
 import LifeSayings from "@utils/json/LifeSayings";
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import { Book, Container, RepresentBook } from "./styles";
 
 const LifeBooks = () => {
+  const slider = useRef<HTMLDivElement>(null);
+  const startPointer = useRef(1);
+  const scrollLeft = useRef(1);
+  const isDown = useRef(false);
+
+  const onMouseDown = useCallback(
+    (e) => {
+      if (slider.current && startPointer.current) {
+        slider.current.classList.add("active");
+        startPointer.current = e.pageX - slider.current.offsetLeft;
+        scrollLeft.current = slider.current.scrollLeft;
+        isDown.current = true;
+      }
+    },
+    [slider.current, startPointer.current]
+  );
+
+  const onMouseUp = useCallback(() => {
+    if (slider.current) {
+      slider.current.classList.remove("active");
+      isDown.current = false;
+    }
+  }, [slider.current]);
+
+  const onMouseLeave = useCallback(() => {
+    if (slider.current) {
+      slider.current.classList.remove("active");
+      isDown.current = false;
+    }
+  }, [slider.current]);
+
+  const onMouseMove = useCallback(
+    (e) => {
+      if (!isDown.current) return;
+
+      e.preventDefault();
+      if (slider.current) {
+        const x = e.pageX - slider.current.offsetLeft;
+
+        const walk = (x - startPointer.current) * 3;
+        slider.current.scrollLeft = scrollLeft.current - walk;
+      }
+    },
+    [slider.current, startPointer.current, scrollLeft.current]
+  );
+
   return (
     <Container>
-      <RepresentBook></RepresentBook>
-      {LifeSayings.map((item) => (
-        <Book></Book>
-      ))}
+      <div
+        className="wrapper"
+        ref={slider}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseMove={onMouseMove}
+        onMouseLeave={onMouseLeave}
+      >
+        <RepresentBook></RepresentBook>
+        {LifeSayings.map((item) => (
+          <>
+            <Book></Book>
+            <Book>
+              <p>{item.content}</p>
+            </Book>
+          </>
+        ))}
+      </div>
     </Container>
   );
 };
--- src/components/LifeBooks/styles.tsx
+++ src/components/LifeBooks/styles.tsx
@@ -2,53 +2,60 @@ import styled from "@emotion/styled";
 
 export const Container = styled.div`
   width: 100%;
-  height: 100px;
-  position: relative;
-  border: 1px solid black;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .wrapper {
+    perspective: 500px;
+    border: 1px solid red;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    width: 100%;
+    height: 100%;
+
+    cursor: pointer;
+    will-change: transform;
+    white-space: nowrap;
+    transform: scale(0.98);
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+  .active {
+    background: rgba(255, 255, 255, 0.3);
+    cursor: grabbing;
+    cursor: -webkit-grabbing;
+    transform: scale(1);
+  }
 `;
 
 export const RepresentBook = styled.div`
-  width: 100px;
-  height: 100%;
-  position: absolute;
-  top: 0;
+  width: 200px;
+  height: calc(100% - 40px);
+  background-color: yellow;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  transform: scaleX(1.31) rotateY(40deg);
 `;
 
 export const Book = styled.div`
-  width: 100px;
-  height: 100%;
-  position: absolute;
-  left: 0;
+  width: 200px;
+  height: calc(100% - 40px);
   background-color: pink;
-  opacity: 0;
-
-  @keyframes moveOne {
-    0% {
-    }
-    100% {
-      opacity: 1;
-    }
+  border: 1px solid black;
+  display: inline-block;
+  /* justify-content: center;
+  align-items: center; */
+  p {
+    width: 100px;
+    border: 1px solid blue;
   }
-
-  @keyframes moveTwo {
-    0% {
-    }
-    100% {
-    }
+  &:nth-of-type(even) {
+    transform: scaleX(1.31) rotateY(-40deg);
   }
-
-  @keyframes moveThree {
-    0% {
-    }
-    100% {
-    }
+  &:nth-of-type(odd) {
+    transform: scaleX(1.31) rotateY(40deg);
   }
-
-  @keyframes moveFour {
-    0% {
-    }
-    100% {
-    }
-  }
-  animation: moveOne 1s linear forwards;
 `;
